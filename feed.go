b4a830c1ab32657ce413aba88a05d3d39e767efd
Colin Seymour <colin@symr.io>
2021-05-08 12:13:50 +0100

    Update feed item and formatting

--- feed.go
+++ feed.go
@@ -8,31 +8,52 @@ import (
 
 // Feed is a slice of Items representing customer transactions
 type feed struct {
-	Items []Item `json:"feedItems"`
+	Items []FeedItem `json:"feedItems"`
 }
 
 // Item is a single customer transaction in their feed
-type Item struct {
-	FeedItemUID              string    `json:"feedItemUid"`
-	CategoryUID              string    `json:"categoryUid"`
-	Amount                   Amount    `json:"amount"`
-	SourceAmount             Amount    `json:"sourceAmount"`
-	Direction                string    `json:"direction"`
-	TransactionTime          time.Time `json:"transactionTime"`
-	Source                   string    `json:"source"`
-	SourceSubType            string    `json:"sourceSubType"`
-	Status                   string    `json:"status"`
-	CounterPartyType         string    `json:"counterPartyType"`
-	CounterPartyUID          string    `json:"counterPartyUid"`
-	CounterPartySubEntityUID string    `json:"counterPartySubEntityUid"`
-	Reference                string    `json:"reference"`
-	Country                  string    `json:"country"`
-	SpendingCategory         string    `json:"spendingCategory"`
+type FeedItem struct {
+	FeedItemUID                       string      `json:"feedItemUid"`
+	CategoryUID                       string      `json:"categoryUid"`
+	AccountUID                        string      `json:"accountUid"`
+	Amount                            Amount      `json:"amount"`
+	SourceAmount                      Amount      `json:"sourceAmount"`
+	Direction                         string      `json:"direction"`
+	UpdatedAt                         time.Time   `json:"updatedAt"`
+	TransactionTime                   time.Time   `json:"transactionTime"`
+	SettlementTime                    time.Time   `json:"settlementTime"`
+	RetryAllocationUntilTime          time.Time   `json:"retryAllocationUntilTime"`
+	Source                            string      `json:"source"`
+	SourceSubType                     string      `json:"sourceSubType"`
+	Status                            string      `json:"status"`
+	TransactionApplicationUserUID     string      `json:"transactionApplicationUserUid"`
+	CounterPartyType                  string      `json:"counterPartyType"`
+	CounterPartyUID                   string      `json:"counterPartyUid"`
+	CounterPartyName                  string      `json:"counterPartyName"`
+	CounterPartySubEntityUID          string      `json:"counterPartySubEntityUid"`
+	CounterPartySubEntityName         string      `json:"counterPartySubEntityName"`
+	CounterPartySubEntityIdentifier   string      `json:"counterPartySubEntityIdentifier"`
+	CounterPartSubEntitySubIdentifier string      `json:"counterPartSubEntitySubIdentifier"`
+	ExchangeRate                      float64     `json:"exchangeRate"`
+	TotalFees                         float64     `json:"totalFees"`
+	TotalFeeAmount                    Amount      `json:"totalFeeAmount"`
+	Reference                         string      `json:"reference"`
+	Country                           string      `json:"country"`
+	SpendingCategory                  string      `json:"spendingCategory"`
+	UserNote                          string      `json:"userNote"`
+	RoundUp                           FeedRoundUp `json:"roundUp"`
+	HasAttachment                     bool        `json:"hasAttachment"`
+	ReceiptPresent                    bool        `json:"receiptPresent"`
+}
+
+type FeedRoundUp struct {
+	GoalCategoryUID string `json:"goalCategoryUid"`
+	Amount          Amount `json:"amount"`
 }
 
 // Feed returns a slice of Items for a given account and category. It returns an error if unable
 // to retrieve the feed.
-func (c *Client) Feed(ctx context.Context, act, cat string, since time.Time) ([]Item, *http.Response, error) {
+func (c *Client) Feed(ctx context.Context, act, cat string, since time.Time) ([]FeedItem, *http.Response, error) {
 	req, err := c.NewRequest("GET", "/api/v2/feed/account/"+act+"/category/"+cat, nil)
 	if err != nil {
 		return nil, nil, err
@@ -53,13 +74,13 @@ func (c *Client) Feed(ctx context.Context, act, cat string, since time.Time) ([]
 // FeedItem returns a feed Item for a given account and category. It returns an error if unable to
 // retrieve the feed Item.
 // Note: FeedItem uses the v2 API which is still under active development.
-func (c *Client) FeedItem(ctx context.Context, act, cat, itm string) (*Item, *http.Response, error) {
+func (c *Client) FeedItem(ctx context.Context, act, cat, itm string) (*FeedItem, *http.Response, error) {
 	req, err := c.NewRequest("GET", "/api/v2/feed/account/"+act+"/category/"+cat+"/"+itm, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	var i Item
+	var i FeedItem
 	resp, err := c.Do(ctx, req, &i)
 	if err != nil {
 		return nil, resp, err
