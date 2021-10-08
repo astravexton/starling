996832769aa3172a382a64444e1cf87eb8b3c9de
astravexton <squ1dge08@gmail.com>
2021-10-08 18:54:17 +0100

    Remove tests that aren't needed

--- accounts_test.go
+++ accounts_test.go
@@ -183,90 +183,6 @@ func TestAccountIDsForbidden(t *testing.T) {
 	}
 }
 
-var accountTestCases = []struct {
-	name string
-	mock string
-}{
-	{
-		name: "sample account",
-		mock: `{
-			"_links": {
-				"card": {
-					"href": "api/v1/cards",
-					"templated": false
-				},
-				"customer": {
-					"href": "api/v1/customers",
-					"templated": false
-				},
-				"mandates": {
-					"href": "/api/v1/direct-debit/mandates",
-					"templated": false
-				},
-				"payees": {
-					"href": "api/v1/contacts",
-					"templated": false
-				},
-				"transactions": {
-					"href": "api/v1/transactions?from={fromDate}&to={toDate}",
-					"templated": true
-				}
-			},
-			"id": "6f5a3548-f25d-4dfe-9f8e-3078fe8bfa2a",
-			"name": "6d2aa528-b9d1-4083-ae7c-53d460cd8d88 GBP",
-			"number": "04829435",
-			"accountNumber": "04829435",
-			"sortCode": "608371",
-			"currency": "GBP",
-			"iban": "GB28SRLG60837104829435",
-			"bic": "SRLGGB2L",
-			"createdAt": "2017-03-09T17:58:15.848Z"
-		}`,
-	},
-	{
-		name: "sample account without HAL wrapper",
-		mock: `{
-			"id": "6f5a3548-f25d-4dfe-9f8e-3078fe8bfa2a",
-			"name": "6d2aa528-b9d1-4083-ae7c-53d460cd8d88 GBP",
-			"number": "04829435",
-			"accountNumber": "04829435",
-			"sortCode": "608371",
-			"currency": "GBP",
-			"iban": "GB28SRLG60837104829435",
-			"bic": "SRLGGB2L",
-			"createdAt": "2017-03-09T17:58:15.848Z"
-		}`,
-	},
-}
-
-func TestAccount(t *testing.T) {
-	for _, tc := range accountTestCases {
-		t.Run(tc.name, func(st *testing.T) {
-			testAccount(st, tc.name, tc.mock)
-		})
-	}
-}
-
-func testAccount(t *testing.T, name, mock string) {
-	client, mux, _, teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/api/v2/accounts", func(w http.ResponseWriter, r *http.Request) {
-		checkMethod(t, r, http.MethodGet)
-		fmt.Fprint(w, mock)
-	})
-
-	got, _, err := client.Account(context.Background())
-	checkNoError(t, err)
-
-	want := &Account{}
-	json.Unmarshal([]byte(mock), want)
-
-	if !reflect.DeepEqual(got, want) {
-		t.Error("should return an account matching the mock response", cross)
-	}
-}
-
 var balanceTestCases = []struct {
 	name string
 	mock string
@@ -348,27 +264,6 @@ func testAccountBalance(t *testing.T, name, mock string) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Error("should return an account balance matching the mock response", cross)
-	}
-}
-
-func TestAccountForbidden(t *testing.T) {
-	client, mux, _, teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/api/v2/accounts", func(w http.ResponseWriter, r *http.Request) {
-		checkMethod(t, r, http.MethodGet)
-		w.WriteHeader(http.StatusForbidden)
-	})
-
-	got, resp, err := client.Account(context.Background())
-	checkHasError(t, err)
-
-	if resp.StatusCode != http.StatusForbidden {
-		t.Error("should return HTTP 403 status")
-	}
-
-	if got != nil {
-		t.Error("should not return an account")
 	}
 }
 
